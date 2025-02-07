2da532cf83bfdaf8d4191293fa477d6f64ad6bef
Ben Boyle <benboyle@Bens-MacBook-Air.local>
2025-02-06 17:47:08 -0700

    team 4

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import productList from "./productList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
+loadHeaderFooter();
 // console.log("Hello from main.js");
 console.log(productList("tents", ".product-list"));
