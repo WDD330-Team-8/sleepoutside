b5c9dc0245c9324dfb3fdce4fb07825f36d738fc
randiumphrey <randiumphrey@byui.edu>
2025-02-28 16:45:25 -0700

    Team assigment 7 - work in progress. Need to fix 400 error with form.

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,10 +1,11 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
-function convertToJson(res) {
+async function convertToJson(res) {
+  const data = await res.json();
   if (res.ok) {
-    return res.json();
+    return data;
   } else {
-    throw new Error("Bad Response");
+    throw { name:"servicesError", message: data };
   }
 }
 
@@ -28,5 +29,5 @@ export async function checkout(payload) {
     },
     body: JSON.stringify(payload),
   };
-  return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
+  return await fetch(baseURL + "checkout/", options).then(convertToJson);
 }
